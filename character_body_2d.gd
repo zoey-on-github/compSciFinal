0f2750b8508c93007ea02f56fec5894a6c4027c1
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-05-23 03:48:58 -0700

    2 players

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -900.0
 
-const SPEED = 300.0
+const SPEED = 500.0
 const inputs = {}
 const tile_size = 64
 func _physics_process(delta):
--- second_character_body_2d.gd
+++ second_character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 const JUMP_VELOCITY = -900.0
 
-const SPEED = 1500.0
+const SPEED = 500.0
 const inputs = {}
 const tile_size = 64
 func _physics_process(delta):
@@ -9,17 +9,15 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump2") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("move_left", "move_right")
-	if Input.is_action_pressed("move_left"):
-		velocity.x = SPEED
-	if Input.is_action_pressed("move_right"):
-		velocity.x = -SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_just_pressed("ground_pound") and !is_on_floor():
+	var direction = Input.get_axis("move_left2", "move_right2")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_pressed("ground_pound2") and !is_on_floor():
 		velocity.y = JUMP_VELOCITY * -1
 	move_and_slide()
